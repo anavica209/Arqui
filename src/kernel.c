434fefa440a2d36a70291db4fc3e132e43cecd65
Esteban Ordano <esteban@ordano.com.ar>
2010-11-01 22:15:56 -0300

    Apretas teclas y "pasa" algogit add .

--- src/kernel.c
+++ src/kernel.c
@@ -23,7 +23,7 @@ kmain()
 /* CARGA DE IDT CON LA RUTINA DE ATENCION DE IRQ0    */
 
         setup_IDT_entry (&idt[0x08], 0x08, (dword)&_int_08_hand, ACS_INT, 0);
-        setup_IDT_entry (&idt[0x08], 0x09, (dword)&_int_09_hand, ACS_INT, 0);
+        setup_IDT_entry (&idt[0x09], 0x09, (dword)&_int_09_hand, ACS_INT, 0);
 	
 /* Carga de IDTR    */
 
