14c7188aa6e9613098dde848218b67696182dab1
Esteban Ordano <esteban@ordano.com.ar>
2010-11-03 20:26:38 -0300

    Movida función que tenía que ir en kmain.c

--- src/kmain.c
+++ src/kmain.c
@@ -5,6 +5,26 @@
 DESCR_INT idt[0x81];			/* IDT de 10 entradas*/
 IDTR idtr;				/* IDTR */
 
+/***************************************************************
+*setup_IDT_entry
+* Inicializa un descriptor de la IDT
+*
+*Recibe: Puntero a elemento de la IDT
+*	 Selector a cargar en el descriptor de interrupcion
+*	 Puntero a rutina de atencion de interrupcion	
+*	 Derechos de acceso del segmento
+*	 Cero
+****************************************************************/
+
+static void setup_IDT_entry (DESCR_INT *item, byte selector, dword offset, byte access,
+			 byte cero) {
+  item->selector = selector;
+  item->offset_l = offset & 0xFFFF;
+  item->offset_h = offset >> 16;
+  item->access = access;
+  item->cero = cero;
+}
+
 /**********************************************
 kmain() 
 Punto de entrada de código C.
